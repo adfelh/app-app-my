bbb978a9b1f7d183e0d8e56efaea4b2380f1eb65
adfelh <142008759+adfelh@users.noreply.github.com>
2024-05-21 16:35:44 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Oswald, Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-import Footer from "@/components/footer";
+
 
 
 const oswald = Oswald({ subsets: ["latin"], weight: ['200', '300', '400', '500', '600', '700', ], 
@@ -28,7 +28,7 @@ export default function RootLayout({
         <Header />
         
         {children}</body>
-        <Footer />
+        
     </html>
   );
 }
